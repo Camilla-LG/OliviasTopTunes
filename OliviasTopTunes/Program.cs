aff92a7dbfdda447c9f16a0e743e9c6da0a5b409
camillalo1 <camillalo@getacademy.no>
2023-12-01 11:36:05 +0100

    Add project files.

---
+++ OliviasTopTunes/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+MainMenu();
+void MainMenu()
+{
+    Console.WriteLine("Velkommen til Olivias favoritter!");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine();
+    Console.WriteLine(" *** Meny *** ");
+    Console.WriteLine();
+    Console.WriteLine("1. Vis sanger.");
+    Console.WriteLine("2. Legg til ny sang.");
+    Console.WriteLine("3. Slett sang.");
+}
---
+++ OliviasTopTunes/Songs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OliviasTopTunes
+{
+    internal class Songs
+    {
+        public string Band { get; set; }
+        public string Song { get; set; }
+      
+
+        
+        public void SongInfo(string band, string song)
+        {
+            Band = band;
+            Song = song;
+        }
+
+        public AddSongs(string band, string song)
+        {
+            var ListOfSongs = new List<SongInfo>();
+        }
+
+        public void DisplaySongInfo(string band, string song)
+        {
+            for (var i = 0; i < Band.Length; i++) { }
+            {
+                Console.WriteLine($"{song} av {band}");
+            }
+        }
+    }
+}
